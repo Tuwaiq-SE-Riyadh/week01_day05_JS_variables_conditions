0d8010129afb36c6bd1973a99bc80222b5f26b8c
Amal <amalgassam@hotmail.com>
2021-10-07 13:13:53 +0300

    FirstLabJS

---
+++ main.js
@@ -0,0 +1,14 @@
+function question1(X ,Y ,N ,Z){
+ console.log("You will be a "+Z +" in "+N+", and married to "+ Y +" with "+ X +" kids. ");
+}
+
+function question2( CurrentYear,BirthYear ){
+    console.log(CurrentYear-BirthYear)
+}
+
+function question3( x ){
+    z=x*3.14;
+    h=x*3.14*2;
+    console.log("circumference"+z)
+    console.log("area"+h)
+}
