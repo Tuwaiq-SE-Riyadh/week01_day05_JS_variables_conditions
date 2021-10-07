603e1e29d3113f6a3b9518201289f2bfedcc6e7e
aaldosari1 <dosr@live.com>
2021-10-07 15:07:59 +0300

    msg

---
+++ VariablesHW.js
@@ -0,0 +1,21 @@
+function task1(NoC, ParNam, Loc, Job){
+
+console.log("You will be "+Job+" in "+Loc+" and married to "+ ParNam + " with "+NoC+" children")
+
+}
+
+function task2(CurYer, BirYer){
+
+    console.log("your age is " +(CurYer-BirYer))
+    
+    }
+
+    function task3(r){
+  //formula source https://www.omnicalculator.com/math/circumference
+        console.log("The circumference is " +(2*Math.PI)* r )
+        
+        }
+
+
+
+
