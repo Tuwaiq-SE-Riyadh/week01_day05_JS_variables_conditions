2c713f470b1617e75f2210ee385e74f74c53f87e
“[Shrooq]” <[Shroog713@gmail.com]>
2021-10-07 15:41:03 +0300

    lab1,2

---
+++ conditional.js
@@ -0,0 +1,8 @@
+var languages = "Arabic"; 
+if (languages === "English"){ 
+    console.log("Hello World"); 
+}else if (languages === "Arabic"){
+    console.log("مرحبا بالعالم"); 
+}else if (languages === "French"){
+     console.log("Bonjour"); 
+}
