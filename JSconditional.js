b1c0e1d93d073b4a08ebeaf8f64b04bcf4ed3237
Nouraoqail <Nouraoqail.10@gmail.com>
2021-10-07 16:35:35 +0300

    JS variables&conditions update

---
+++ JSconditional.js
@@ -0,0 +1,9 @@
+function condition(language){
+if(language== "English"){
+    console.log("Hello world")
+}else if(language=="Arabic"){
+console.log("مرحبا بالعالم")
+}else if(language=="French"){
+console.log("Bonjour")
+}
+}
