d706f0cb49608db87ab5d2d220d76d900d5ef78b
S-MW <72529306+S-MW@users.noreply.github.com>
2021-10-10 01:21:06 +0300

    solve variables_hw

---
+++ myJs.js
@@ -0,0 +1,17 @@
+function fortuneTeller(childrenNumber,partnerName,geographicLoction, jobTitle )
+{
+    console.log("They have :" ,childrenNumber , "of childern and your partnerName is :" ,partnerName , " in :" ,geographicLoction ," and your job is :" ,jobTitle);
+}
+
+function ageCalculator(birthYear)
+{
+    let currntYear = new Date().getFullYear()
+    console.log("They are" , currntYear-birthYear , "years old.");
+
+}
+
+function getCircumferenceAndArea(radius)
+{
+    console.log("circumference is :",2*3.14*radius );
+    console.log("Area is :",3.14 * radius*radius );
+}
